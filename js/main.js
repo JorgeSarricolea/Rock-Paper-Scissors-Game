25997f83de6cde8c1e3459cc37157ab7dfa550a2
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-09-19 15:03:28 -0600

    fix: logical and conditional to show the result of the game in the js

--- js/main.js
+++ js/main.js
@@ -1,34 +1,84 @@
-// Funtion to get random choise by the PC
+// Function to get a random choice by the computer
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * 3);
   return choices[randomIndex];
 }
 
-// Function to get the status of the game
-function determineWinner(userChoice, computerChoice) {
-  if (userChoice === computerChoice) {
-    return "TIE";
-  } else if (
-    (userChoice === "rock" && computerChoice === "scissors") ||
-    (userChoice === "paper" && computerChoice === "rock") ||
-    (userChoice === "scissors" && computerChoice === "paper")
-  ) {
-    return "YOU WON!";
-  } else {
-    return "JAVASCRIPT WON";
+// Main function to play the game
+function playGame(userChoice) {
+  const computerChoice = getComputerChoice();
+  toggleOptions(userChoice, computerChoice);
+}
+
+// Function to hide and show the options
+function toggleOptions(userChoice, computerChoice) {
+  const options = ["rock", "paper", "scissors"];
+  options.forEach(option => {
+    const button = document.getElementById(option);
+    button.style.display = "none";
+  });
+
+  // Show the user choice
+  const userChoiceButton = document.getElementById(userChoice);
+  userChoiceButton.style.display = "inline-block";
+  console.log("User choice:", userChoice)
+
+  // Show the computer choice
+  console.log("Computer choice:", computerChoice)
+  const pcChoiceRock = document.getElementById("pc-rock");
+  const pcChoicePaper = document.getElementById("pc-paper");
+  const pcChoiceScissors = document.getElementById("pc-scissors");
+
+  if (computerChoice === "rock") {
+    pcChoiceRock.style.display = "inline-block";
+  }
+  else if (computerChoice === "paper") {
+    pcChoicePaper.style.display = "inline-block";
+  }
+  else if (computerChoice === "scissors") {
+    pcChoiceScissors.style.display = "inline-block";
+  }
+
+  // Conditions if computer wins
+
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    console.log("Computer won!");
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    console.log("Computer won!");
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    console.log("Computer won!");
+  }
+
+  // Conditions if user wins
+
+  if (computerChoice === "rock" && userChoice === "paper") {
+    console.log("You won!");
+  }
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    console.log("You won!");
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    console.log("You won!");
+  }
+
+  // Conditions if tie
+
+  if (computerChoice === "rock" && userChoice === "rock") {
+    console.log("Tie!");
+  }
+  if (computerChoice === "paper" && userChoice === "paper") {
+    console.log("Tie!");
+  }
+  if (computerChoice === "scissors" && userChoice === "scissors") {
+    console.log("Tie!");
   }
 }
 
-// Main Function to play the game
-function playGame(userChoice) {
-  const computerChoice = getComputerChoice();
-  const result = determineWinner(userChoice, computerChoice);
-  const resultMessage = `You chose ${userChoice}, JavaScript chose ${computerChoice}. ${result}`;
-  document.querySelector(".result").textContent = resultMessage;
-}
 
-// Functions for each buttons
+// Function for each button
 document.getElementById("rock").addEventListener("click", () => {
   playGame("rock");
 });
