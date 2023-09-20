5977f5b35efac00de47628837367c8d3f723fcec
Jorge Sarricolea <jjorgesarricolea18@gmail.com>
2023-09-20 16:36:42 -0600

    feat: Show the result of the game message

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
+// Global choices
 const options = ["rock", "paper", "scissors"];
+
 // Function to get a random choice by the computer
 function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * 3);
@@ -7,7 +9,6 @@ function getComputerChoice() {
 
 // Main function to play the game
 function playGame(userChoice) {
-  // const computerChoice = getComputerChoice();
   const n = getComputerChoice();
   toggleOptions(userChoice, n);
   gameResults(userChoice, n);
@@ -23,61 +24,68 @@ function toggleOptions(userChoice, computerChoice) {
     // Show the user choice
     const userChoiceButton = document.getElementById(options[userChoice]);
     userChoiceButton.style.display = "inline-block";
-    console.log("User choice:", userChoice)
   });
 
   // Show the computer choice
-  console.log("Computer choice:", computerChoice)
   const pcChoiceRock = document.getElementById("pc-rock");
   const pcChoicePaper = document.getElementById("pc-paper");
   const pcChoiceScissors = document.getElementById("pc-scissors");
 
   const pcResults = [pcChoiceRock, pcChoicePaper, pcChoiceScissors];
-  pcResults[computerChoice].style.display = "flex"
+  pcResults[computerChoice].style.display = "flex";
 }
 
+// Function to determine the result of the game
 function gameResults(userChoice, computerChoice) {
+  const userWonMessage = "you won!";
+  const computerWonMessage = "computer won!";
+  const tieMessage = "tie!";
+  const showMesage = document.getElementById("result-text");
   if (computerChoice === userChoice) {
-    console.log("Tie!");
+    document.body.classList.toggle("tie");
+    showMesage.textContent = tieMessage.toUpperCase();
   } else {
     switch (computerChoice) {
       case 0:
         if (userChoice === 1) {
-          console.log("You won!");
           document.body.classList.toggle("user-won");
+          showMesage.textContent = userWonMessage.toUpperCase();
         } else {
-          console.log("Computer won!");
           document.body.classList.toggle("computer-won");
+          showMesage.textContent = computerWonMessage.toUpperCase();
         }
         break;
       case 1:
         if (userChoice === 2) {
-          console.log("You won!");
           document.body.classList.toggle("user-won");
+          showMesage.textContent = userWonMessage.toUpperCase();
         } else {
-          console.log("Computer won!");
           document.body.classList.toggle("computer-won");
+          showMesage.textContent = computerWonMessage.toUpperCase();
         }
         break;
       case 2:
         if (userChoice === 0) {
-          console.log("You won!");
           document.body.classList.toggle("user-won");
+          showMesage.textContent = userWonMessage.toUpperCase();
         } else {
-          console.log("Computer won!");
           document.body.classList.toggle("computer-won");
+          showMesage.textContent = computerWonMessage.toUpperCase();
         }
         break;
     }
   }
 }
 
+// Funtion to display the results of the game
 function showResults(action, n) {
   document.getElementById(action).addEventListener("click", () => {
     const isClickable = document.getElementById(action).dataset.clickable === "true";
     if (isClickable) {
       playGame(n);
     }
+    // To show the restart button
+    document.getElementById("restart").style.display = "flex";
   });
 }
 
